28a2dcee629bb3aa31a958af2311685828ac054e
Hong-Thai Nguyen <hnguyen@dhatim.com>
2020-12-08 09:38:51 +0100

    Prepare release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.4'
+version = '2.5'
 
 long_description = open('README.rst').read()
 
@@ -14,7 +14,7 @@ setup(
       author='Dhatim',
       author_email='contact@dhatim.com',
       url='https://github.com/dhatim/python-cfonb',
-      download_url = 'https://github.com/dhatim/python-cfonb/archive/2.4.tar.gz',
+      download_url = 'https://github.com/dhatim/python-cfonb/archive/2.5.tar.gz',
       license='LGPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
